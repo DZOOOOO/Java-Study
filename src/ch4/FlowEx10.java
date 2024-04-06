1e3c887a36bc5d731350f6fc47e7042fc386f9a6
DZOOOOO <dzpro0327@naver.com>
2024-04-06 20:08:21 +0900

    chapter04

---
+++ src/ch4/FlowEx10.java
@@ -0,0 +1,25 @@
+package ch4;
+
+import java.util.Scanner;
+
+public class FlowEx10 {
+    public static void main(String[] args) {
+        int score = 0;
+        char grade = ' ';
+
+        System.out.print("당신의 점수를 입력하세요.(1~100)>");
+
+        Scanner scanner = new Scanner(System.in);
+        String tmp = scanner.nextLine();
+        score = Integer.parseInt(tmp);
+
+        grade = switch (score / 10) {
+            case 10, 9 -> 'A';
+            case 8 -> 'B';
+            case 7 -> 'C';
+            default -> 'F';
+        };
+
+        System.out.println("당신의 학점은 " + grade + "입니다.");
+    }
+}
