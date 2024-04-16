5258ef3da795af48b380cae760277b4ee57d9fb3
DZOOOOO <dzpro0327@naver.com>
2024-04-17 02:00:57 +0900

    chapter12

---
+++ src/jungsuck/ch12/AnnotationEx2.java
@@ -0,0 +1,26 @@
+package jungsuck.ch12;
+
+public class AnnotationEx2 {
+    public static void main(String[] args) {
+        NewClass nc = new NewClass();
+
+        nc.oldField = 10;
+        System.out.println(nc.getOldField());
+    }
+}
+
+class NewClass {
+    int newField;
+
+    int getNewField() {
+        return newField;
+    }
+
+    @Deprecated
+    int oldField;
+
+    @Deprecated
+    int getOldField() {
+        return oldField;
+    }
+}
---
+++ src/jungsuck/ch12/FruitBoxEx2.java
@@ -0,0 +1,73 @@
+package jungsuck.ch12;
+
+import java.util.ArrayList;
+
+public class FruitBoxEx2 {
+    public static void main(String[] args) {
+        FruitBox1<Fruit1> fruitBox = new FruitBox1<>();
+        FruitBox1<Apple1> appleBox = new FruitBox1<>();
+        FruitBox1<Grape1> grapeBox = new FruitBox1<>();
+
+        fruitBox.add(new Fruit1());
+        fruitBox.add(new Apple1());
+        fruitBox.add(new Grape1());
+
+        appleBox.add(new Apple1());
+        grapeBox.add(new Grape1());
+
+        System.out.println("fruitBox - " + fruitBox);
+        System.out.println("appleBox - " + appleBox);
+        System.out.println("grapeBox - " + grapeBox);
+    }
+}
+
+class Fruit1 implements Eatable {
+    public String toString() {
+        return "Fruit";
+    }
+}
+
+class Apple1 extends Fruit1 {
+    public String toString() {
+        return "Apple";
+    }
+}
+
+class Grape1 extends Fruit1 {
+    public String toString() {
+        return "Grape";
+    }
+}
+
+class Toy1 extends Fruit1 {
+    public String toString() {
+        return "Toy";
+    }
+}
+
+interface Eatable {
+}
+
+class FruitBox1<T extends Fruit1 & Eatable> extends Box1<T> {
+
+}
+
+class Box1<T> {
+    ArrayList<T> list = new ArrayList<>();
+
+    void add(T item) {
+        list.add(item);
+    }
+
+    T get(int i) {
+        return list.get(i);
+    }
+
+    int size() {
+        return list.size();
+    }
+
+    public String toString() {
+        return list.toString();
+    }
+}
