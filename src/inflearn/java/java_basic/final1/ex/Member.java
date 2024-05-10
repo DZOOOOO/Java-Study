91d71634e7460f38a79694f2c34cd431293f8747
DZOOOOO <dzpro0327@naver.com>
2024-05-10 15:38:27 +0900

    final add

---
+++ src/inflearn/java/java_basic/final1/ex/Member.java
@@ -0,0 +1,21 @@
+package inflearn.java.java_basic.final1.ex;
+
+public class Member {
+
+    private final String id;
+    private String name;
+
+    public Member(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void changeData(String id, String name) {
+//        this.id = id; 변경 불가능
+        this.name = name;
+    }
+
+    public void print() {
+        System.out.println("id : " + id + ", name : " + name);
+    }
+}
