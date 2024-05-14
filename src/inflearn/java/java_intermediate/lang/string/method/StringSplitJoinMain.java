e2217b02aba38833375fe1c194aa5537d7738838
DZOOOOO <dzpro0327@naver.com>
2024-05-14 20:23:48 +0900

    String add

---
+++ src/inflearn/java/java_intermediate/lang/string/method/StringSplitJoinMain.java
@@ -0,0 +1,29 @@
+package inflearn.java.java_intermediate.lang.string.method;
+
+public class StringSplitJoinMain {
+    public static void main(String[] args) {
+        String str = "Apple,Banana,Orange";
+
+        // split()
+        String[] split = str.split(",");
+        for (String s : split) {
+            System.out.println(s);
+        }
+
+        String joinStr = "";
+        for (String string : split) {
+            joinStr += string + "-";
+        }
+
+        System.out.println("joinStr = " + joinStr);
+
+        // join()
+        String joinedStr = String.join("-", "A", "B", "C");
+        System.out.println("연결된 문자열 = " + joinedStr);
+
+
+        // 문자열 배열 연결
+        String result = String.join("-", split);
+        System.out.println("result = " + result);
+    }
+}
---
+++ src/inflearn/java/java_intermediate/lang/string/test/TestString6.java
@@ -0,0 +1,16 @@
+package inflearn.java.java_intermediate.lang.string.test;
+
+public class TestString6 {
+    public static void main(String[] args) {
+        String str = "start hello java, hello spring, hello jpa";
+        String key = "hello";
+
+        int count = 0;
+        int index = str.indexOf(key);
+        while (index >= 0) {
+            index = str.indexOf(key, index + 1);
+            count += 1;
+        }
+        System.out.println("count = " + count);
+    }
+}
