f68c81857ca063132e33437114378d027d0dfd5e
DZOOOOO <dzpro0327@naver.com>
2024-05-16 00:03:46 +0900

    DateTime add

---
+++ src/inflearn/java/java_intermediate/time/IsSupportedMain1.java
@@ -0,0 +1,12 @@
+package inflearn.java.java_intermediate.time;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoField;
+
+public class IsSupportedMain1 {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+        int minute = now.get(ChronoField.SECOND_OF_MINUTE); // 지원하지 않는 필드.
+        System.out.println("minute = " + minute);
+    }
+}
---
+++ src/inflearn/java/java_intermediate/time/test/TestLoopPlus.java
@@ -0,0 +1,14 @@
+package inflearn.java.java_intermediate.time.test;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class TestLoopPlus {
+    public static void main(String[] args) {
+        LocalDate startDate = LocalDate.of(2024, 1, 1);
+        for (int i = 1; i <= 5; i++) {
+            LocalDate nextDate = startDate.plus(2 * i, ChronoUnit.WEEKS);
+            System.out.println("날짜 " + i + ": " + nextDate);
+        }
+    }
+}
